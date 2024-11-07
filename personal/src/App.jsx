d82fee7e66234aa52179108e5fb48edac91df30c
kamau <githuakamau99@gmail.com>
2024-11-07 19:41:49 +0300

    feat(navbar): add responsive navbar with logo and navigation links

--- personal/src/App.jsx
+++ personal/src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-red-500'> hello world</h1>
+      <Navbar />
+      <div className="max-w-screen-xl mx-auto px-16">
+        {/* <h1 className="text-center text-2xl mt-8">Welcome to My Site</h1> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
